8484e6c6b2626a92b717e40eef4382aa628e998a
Master Programmer Otacon <mpotacon@aol.com>
2012-12-17 13:34:06 -0500

    Added ChestTest
    use /chest should open inventory - still need to test

--- src/com/github/mpotacon/ironopen/ironopen.java
+++ src/com/github/mpotacon/ironopen/ironopen.java
@@ -3,8 +3,10 @@ package com.github.mpotacon.ironopen;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class ironopen extends JavaPlugin {
+
 	public void onEnable(){
 		getServer().getPluginManager().registerEvents(new irondoorListener(), this);
 		getCommand("open").setExecutor(new irondoorCommands());
+		getCommand("chest").setExecutor(new ChestTest());
 	}
 }
